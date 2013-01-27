91f95309f60f9990f7045bb88328d476a91018c8
Mikko Forsström <puhemies@diktaattoriporssi.com>
2013-01-27 13:55:14 +0200

    Added convenience stuff.

--- tests/Xi/Filelib/Tests/TestCase.php
+++ tests/Xi/Filelib/Tests/TestCase.php
@@ -2,7 +2,50 @@
 
 namespace Xi\Filelib\Tests;
 
+use Xi\Filelib\Configuration;
+
 class TestCase extends \PHPUnit_Framework_TestCase
 {
+    public function getMockedFileOperator()
+    {
+        $fileop = $this
+            ->getMockBuilder('Xi\Filelib\File\FileOperator')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $fileop;
+    }
+
+    public function getMockedStorage()
+    {
+        return $this->getMock('Xi\Filelib\Storage\Storage');
+    }
+
+    public function getMockedAcl()
+    {
+        return $this->getMock('Xi\Filelib\Acl\Acl');
+    }
+
+    public function getMockedEventDispatcher()
+    {
+        return $this->getMock('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+    }
+
+    public function getMockedPublisher()
+    {
+        return $this->getMock('Xi\Filelib\Publisher\Publisher');
+    }
+
+    public function getConfigurationWithMockedObjects()
+    {
+        $configuration = new Configuration();
+        $configuration->setAcl($this->getMockedAcl());
+        $configuration->setPublisher($this->getMockedPublisher());
+        $configuration->setEventDispatcher($this->getMockedEventDispatcher());
+        $configuration->setStorage($this->getMockedStorage());
+
+        return $configuration;
+    }
+
 
 }
