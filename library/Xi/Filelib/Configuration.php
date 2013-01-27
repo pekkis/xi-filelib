15ae34973da6c5272bd63f3814a373bd4b059622
Mikko Forsström <puhemies@diktaattoriporssi.com>
2013-01-27 12:29:22 +0200

    Added conf object and tests.

---
+++ library/Xi/Filelib/Configuration.php
@@ -0,0 +1,283 @@
+<?php
+
+/**
+ * This file is part of the Xi Filelib package.
+ *
+ * For copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xi\Filelib;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Xi\Filelib\Backend\Backend;
+use Xi\Filelib\Storage\Storage;
+use Xi\Filelib\Publisher\Publisher;
+use Xi\Filelib\Acl\Acl;
+use Xi\Filelib\Queue\Queue;
+use Xi\Filelib\IdentityMap\IdentityMap;
+use Xi\Filelib\Backend\Platform\Platform;
+use Xi\Filelib\Exception\InvalidArgumentException;
+
+class Configuration
+{
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    /**
+     * @var Backend
+     */
+    private $backend;
+
+    /**
+     * @var Storage
+     */
+    private $storage;
+
+    /**
+     * @var Publisher
+     */
+    private $publisher;
+
+    /**
+     * @var Acl
+     */
+    private $acl;
+
+    /**
+     * @var string
+     */
+    private $tempDir;
+
+    /**
+     * @var Queue
+     */
+    private $queue;
+
+    /**
+     * @var IdentityMap
+     */
+    private $identityMap;
+
+    /**
+     * @var Platform
+     */
+    private $platform;
+
+    /**
+     * @param EventDispatcherInterface $eventDispatcher
+     * @return Configuration
+     */
+    public function setEventDispatcher(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        return $this;
+    }
+
+    /**
+     * @return EventDispatcherInterface
+     */
+    public function getEventDispatcher()
+    {
+        if (!$this->eventDispatcher) {
+            $this->eventDispatcher = new EventDispatcher();
+        }
+        return $this->eventDispatcher;
+    }
+
+
+
+    /**
+     * Sets temporary directory
+     *
+     * @param string $tempDir
+     * @return Configuration
+     */
+    public function setTempDir($tempDir)
+    {
+        if (!is_dir($tempDir) || !is_writable($tempDir)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Temp dir "%s" is not writable or does not exist',
+                    $tempDir
+                )
+            );
+        }
+        $this->tempDir = $tempDir;
+        return $this;
+    }
+
+    /**
+     * Returns temporary directory
+     *
+     * @return string
+     */
+    public function getTempDir()
+    {
+        if (!$this->tempDir) {
+            $this->setTempDir(sys_get_temp_dir());
+        }
+
+        return $this->tempDir;
+    }
+
+    /**
+     * Sets storage
+     *
+     * @param Storage $storage
+     * @return Configuration
+     */
+    public function setStorage(Storage $storage)
+    {
+        $this->storage = $storage;
+
+        return $this;
+    }
+
+    /**
+     * Returns storage
+     *
+     * @return Storage
+     */
+    public function getStorage()
+    {
+        return $this->storage;
+    }
+
+    /**
+     * Sets publisher
+     *
+     * @param Publisher $publisher
+     * @return Configuration
+     */
+    public function setPublisher(Publisher $publisher)
+    {
+        $this->publisher = $publisher;
+        return $this;
+    }
+
+    /**
+     * Returns publisher
+     *
+     * @return Publisher
+     */
+    public function getPublisher()
+    {
+        return $this->publisher;
+    }
+
+    /**
+     * Sets backend
+     *
+     * @param Backend $backend
+     * @return Configuration
+     */
+    public function setBackend(Backend $backend)
+    {
+        $this->backend = $backend;
+        return $this;
+    }
+
+    /**
+     * Returns backend
+     *
+     * @return Backend
+     */
+    public function getBackend()
+    {
+        return $this->backend;
+    }
+
+    /**
+     * Sets acl handler
+     *
+     * @param Acl $acl
+     * @return Configuration
+     */
+    public function setAcl(Acl $acl)
+    {
+        $this->acl = $acl;
+        return $this;
+    }
+
+    /**
+     * Returns acl handler
+     *
+     * @return Acl
+     */
+    public function getAcl()
+    {
+        return $this->acl;
+    }
+
+    /**
+     * Sets queue
+     *
+     * @param Queue $queue
+     * @return Configuration
+     */
+    public function setQueue(Queue $queue)
+    {
+        $this->queue = $queue;
+        return $this;
+    }
+
+    /**
+     * Returns queue
+     *
+     * @return Queue
+     */
+    public function getQueue()
+    {
+        return $this->queue;
+    }
+
+    /**
+     * Sets identity map
+     *
+     * @param IdentityMap $identityMap
+     * @return Configuration
+     */
+    public function setIdentityMap(IdentityMap $identityMap)
+    {
+        $this->identityMap = $identityMap;
+        return $this;
+    }
+
+    /**
+     * Returns identity map
+     *
+     * @return IdentityMap
+     */
+    public function getIdentityMap()
+    {
+        return $this->identityMap;
+    }
+
+    /**
+     * Sets platform
+     *
+     * @param Platform $platform
+     * @return Configuration
+     */
+    public function setPlatform(Platform $platform)
+    {
+        $this->platform = $platform;
+        return $this;
+    }
+
+    /**
+     * Returns platform
+     *
+     * @return Platform
+     */
+    public function getPlatform()
+    {
+        return $this->platform;
+    }
+
+
+}
---
+++ tests/Xi/Filelib/Tests/ConfigurationTest.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Xi\Filelib\Tests;
+
+use Xi\Filelib\Configuration;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->dirname = ROOT_TESTS . '/data/publisher/unwritable_dir';
+        chmod($this->dirname, 0444);
+    }
+
+    public function tearDown()
+    {
+        chmod($this->dirname, 0755);
+    }
+
+    /**
+     * @test
+     */
+    public function classShouldExist()
+    {
+        $this->assertTrue(class_exists('Xi\Filelib\Configuration'));
+    }
+
+    /**
+     * @test
+     */
+    public function storageSetterAndGetterShouldWorkAsExpected()
+    {
+        $configuration = new Configuration();
+        $storage = $this->getMock('Xi\Filelib\Storage\Storage');
+
+        $this->assertNull($configuration->getStorage());
+        $this->assertSame($configuration, $configuration->setStorage($storage));
+        $this->assertSame($storage, $configuration->getStorage());
+    }
+
+
+    /**
+     * @test
+     */
+    public function publisherSetterAndGetterShouldWorkAsExpected()
+    {
+        $configuration = new Configuration();
+        $obj = $this->getMockForAbstractClass('Xi\Filelib\Publisher\Publisher');
+        $this->assertEquals(null, $configuration->getPublisher());
+        $this->assertSame($configuration, $configuration->setPublisher($obj));
+        $this->assertSame($obj, $configuration->getPublisher());
+    }
+
+
+    /**
+     * @test
+     */
+    public function queueSetterAndGetterShouldWorkAsExpected()
+    {
+        $configuration = new Configuration();
+        $obj = $this->getMock('Xi\Filelib\Queue\Queue');
+        $this->assertEquals(null, $configuration->getQueue());
+        $this->assertSame($configuration, $configuration->setQueue($obj));
+        $this->assertSame($obj, $configuration->getQueue());
+    }
+
+    /**
+     * @test
+     */
+    public function platformSetterAndGetterShouldWorkAsExpected()
+    {
+        $configuration = new Configuration();
+        $obj = $this->getMock('Xi\Filelib\Backend\Platform\Platform');
+        $this->assertEquals(null, $configuration->getPlatform());
+        $this->assertSame($configuration, $configuration->setPlatform($obj));
+        $this->assertSame($obj, $configuration->getPlatform());
+    }
+
+    /**
+     * @test
+     */
+    public function backendSetterAndGetterShouldWorkAsExpected()
+    {
+        $configuration = new Configuration();
+        $obj = $this
+            ->getMockBuilder('Xi\Filelib\Backend\Backend')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->assertEquals(null, $configuration->getBackend());
+        $this->assertSame($configuration, $configuration->setBackend($obj));
+        $this->assertSame($obj, $configuration->getBackend());
+    }
+
+    /**
+     * @test
+     */
+    public function identityMapSetterAndGetterShouldWorkAsExpected()
+    {
+        $configuration = new Configuration();
+        $obj = $this
+            ->getMockBuilder('Xi\Filelib\IdentityMap\IdentityMap')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->assertEquals(null, $configuration->getIdentityMap());
+        $this->assertSame($configuration, $configuration->setIdentityMap($obj));
+        $this->assertSame($obj, $configuration->getIdentityMap());
+    }
+
+    /**
+     * @test
+     */
+    public function aclSetterAndGetterShouldWorkAsExpected()
+    {
+        $configuration = new Configuration();
+        $obj = $this->getMockForAbstractClass('Xi\Filelib\Acl\Acl');
+        $this->assertEquals(null, $configuration->getAcl());
+        $this->assertSame($configuration, $configuration->setAcl($obj));
+        $this->assertSame($obj, $configuration->getAcl());
+    }
+
+    /**
+     * @test
+     */
+    public function tempDirShouldDefaultToSystemTempDir()
+    {
+        $configuration = new Configuration();
+        $this->assertEquals(sys_get_temp_dir(), $configuration->getTempDir());
+    }
+
+    /**
+     * @test
+     */
+    public function setTempDirShouldFailWhenDirectoryDoesNotExists()
+    {
+        $configuration = new Configuration();
+
+        $this->setExpectedException(
+            'InvalidArgumentException',
+            sprintf(
+                'Temp dir "%s" is not writable or does not exist',
+                ROOT_TESTS . '/nonexisting_directory'
+            )
+        );
+
+        $configuration->setTempDir(ROOT_TESTS . '/nonexisting_directory');
+    }
+
+    /**
+     * @test
+     */
+    public function setTempDirShouldFailWhenDirectoryIsNotWritable()
+    {
+        $dirname = ROOT_TESTS . '/data/publisher/unwritable_dir';
+        $this->assertTrue(is_dir($this->dirname));
+        $this->assertFalse(is_writable($this->dirname));
+
+        $configuration = new Configuration();
+
+        $this->setExpectedException(
+            'InvalidArgumentException',
+            sprintf(
+                'Temp dir "%s" is not writable or does not exist',
+                $dirname
+            )
+        );
+
+        $configuration->setTempDir($dirname);
+    }
+
+    /**
+     * @test
+     */
+    public function getEventDispatcherShouldDefaultToSymfonyDefaultImplementation()
+    {
+        $configuration = new Configuration();
+        $dispatcher = $configuration->getEventDispatcher();
+        $this->assertInstanceOf('Symfony\Component\EventDispatcher\EventDispatcher', $dispatcher);
+    }
+
+    /**
+     * @test
+     */
+    public function getEventDispatcherShouldObeySetter()
+    {
+        $configuration = new Configuration();
+
+        $mock = $this->getMockForAbstractClass('Symfony\Component\EventDispatcher\EventDispatcherInterface');
+
+        $this->assertSame($configuration, $configuration->setEventDispatcher($mock));
+
+        $dispatcher = $configuration->getEventDispatcher();
+
+        $this->assertSame($mock, $dispatcher);
+    }
+}
